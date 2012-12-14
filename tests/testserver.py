e59a61590932c1919da7b67c3838576ea0ed7b17
Takafumi Arakaki <aka.tkf@gmail.com>
2012-12-14 20:40:54 +0100

    Rewrite testserver.py using Flask

--- tests/testserver.py
+++ tests/testserver.py
@@ -1,3 +1,28 @@
+from flask import (
+    Flask, request, jsonify)
+
+app = Flask(__name__)
+
+
+@app.route('/report/<path:path>', methods=['GET', 'POST', 'PUT'])
+def report(path):
+    method = request.method
+    params = {}
+    input = None
+
+    if method == 'POST':
+        params = request.form
+    else:
+        input = request.data
+
+    return jsonify(dict(
+        path=path,
+        input=input,
+        params=params,
+        method=method,
+    ))
+
+
 def get_open_port():
     import socket
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -8,44 +33,12 @@ def get_open_port():
     return port
 
 
-def testingapp(environ, start_response):
-    import json
-    import cgi
-
-    method = environ['REQUEST_METHOD']
-    params = {}
-    input = None
-    wsgi_input = environ['wsgi.input']
-
-    size = environ['CONTENT_LENGTH']
-    if size:
-        if method == 'POST':
-            fs = cgi.FieldStorage(environ=environ, fp=wsgi_input)
-            params = dict((k, fs.getlist(k)) for k in fs.keys())
-        else:
-            input = wsgi_input.read(int(size))
-
-    start_response(
-        '200 OK', [('Content-Type', 'application/json')])
-
-    return json.dumps(dict(
-        path=environ['PATH_INFO'],
-        input=input,
-        params=params,
-        method=method,
-    ))
-
-
 def run(port):
     import sys
-    from wsgiref.simple_server import make_server
-
     port = port or get_open_port()
     print port
     sys.stdout.flush()
-
-    server = make_server('', port, testingapp)
-    server.serve_forever()
+    app.run(port=port, debug=True, use_evalex=False)
 
 
 def main(args=None):
