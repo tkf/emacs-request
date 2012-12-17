c83b1125c5a4fcb4fa96529bc396ada96290193c
Takafumi Arakaki <aka.tkf@gmail.com>
2012-12-17 01:04:07 +0100

    Add a test request--curl-preprocess/cookies

--- tests/test-request.el
+++ tests/test-request.el
@@ -279,6 +279,36 @@ RESPONSE-BODY"))
                            :cookies nil
                            :version "1.0" :code 200))))))
 
+(ert-deftest request--curl-preprocess/cookies ()
+  (with-temp-buffer
+    (erase-buffer)
+    (insert "\
+HTTP/1.1 302 Found\r
+Content-Length: 0\r
+Content-Type: text/html; charset=UTF-8\r
+Location: /\r
+Server: TornadoServer/2.2\r
+Set-Cookie: username=Nzk2ZmU; expires=Tue, 15 Jan 2013 22:50:19 GMT; Path=/\r
+\r
+")
+    (insert "\n(:num-redirects 0)")
+    (let ((info (request--curl-preprocess)))
+      (should (equal (buffer-string)
+                     "\
+HTTP/1.1 302 Found
+Content-Length: 0
+Content-Type: text/html; charset=UTF-8
+Location: /
+Server: TornadoServer/2.2
+Set-Cookie: username=Nzk2ZmU; expires=Tue, 15 Jan 2013 22:50:19 GMT; Path=/
+
+"))
+      (should (equal info
+                     (list :num-redirects 0
+                           :redirects nil
+                           :cookies '(("username" . "Nzk2ZmU"))
+                           :version "1.1" :code 302))))))
+
 (provide 'test-request)
 
 ;;; test-request.el ends here
