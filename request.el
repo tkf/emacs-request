135723fb876c6a0e01db26f4d1605d8e9ed02a86
Takafumi Arakaki <aka.tkf@gmail.com>
2012-12-25 16:37:21 +0100

    Copy the first paragraph to request.el from README

--- request.el
+++ request.el
@@ -25,6 +25,13 @@
 
 ;;; Commentary:
 
+;; Request.el is a HTTP request library with multiple backends.  It
+;; supports url.el which is shipped with Emacs and curl command line
+;; program.  User can use curl when s/he has it, as curl is more reliable
+;; than url.el.  Library author can use request.el to avoid imposing
+;; external dependencies such as curl to users while giving richer
+;; experience for users who have curl.
+
 ;; Following functions are adapted from GNU Emacs source code.
 ;; Free Software Foundation holds the copyright of them.
 ;; * `request--process-live-p'
