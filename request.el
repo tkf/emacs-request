bce07a0d81ee9ccfcfb8de5e151ea26dda20d710
dickmao <dick.r.chiang@gmail.com>
2018-11-29 21:45:08 -0500

    Attempt fixing :sync
    See dickmao's comment in #92 regarding why `:sync` does not behave as
    expected.
    
    Previously, `request--curl-sync` would merely return as soon as the
    curl process finished *without regard to
    `#'request--curl-callback`.  This change attaches the semaphore to the
    right place.
    
    (Attempt) Fixes #92

--- request.el
+++ request.el
@@ -1008,7 +1008,7 @@ temporary file paths."
         files))
 
 (cl-defun request--curl (url &rest settings
-                             &key type data files headers timeout response encoding
+                             &key type data files headers timeout response encoding semaphore
                              &allow-other-keys)
   "cURL-based request backend.
 
@@ -1052,7 +1052,9 @@ removed from the buffer before it is shown to the parser function.
     (process-put proc :request-response response)
     (set-process-coding-system proc encoding encoding)
     (set-process-query-on-exit-flag proc nil)
-    (set-process-sentinel proc #'request--curl-callback)))
+    (set-process-sentinel proc #'request--curl-callback)
+    (when semaphore
+      (add-function :after (process-sentinel proc) semaphore))))
 
 (defun request--curl-read-and-delete-tail-info ()
   "Read a sexp at the end of buffer and remove it and preceding character.
@@ -1173,11 +1175,14 @@ START-URL is the URL requested."
   ;; `call-process'.
   (let (finished)
     (prog1 (apply #'request--curl url
-                  :complete (lambda (&rest _) (setq finished t))
+                  :semaphore (lambda (&rest _) (setq finished t))
                   settings)
       (let ((proc (get-buffer-process (request-response--buffer response))))
-        (while (and (not finished) (request--process-live-p proc))
-          (accept-process-output proc))))))
+        (with-local-quit
+          (while (not finished) 
+            (if (request--process-live-p proc)
+                (accept-process-output proc)
+              (sleep-for 0 300))))))))
 
 (defun request--curl-get-cookies (host localpart secure)
   (request--netscape-get-cookies (request--curl-cookie-jar)
