12fadc3842ccd423961256cd4a8b8fe5ddab7aa2
Kien Nguyen <kien.n.quang@gmail.com>
2019-10-13 22:17:57 +0900

    automatically detect curl compression capability

--- request.el
+++ request.el
@@ -887,6 +887,30 @@ Currently it is used only for testing.")
   (or request--curl-cookie-jar
       (expand-file-name "curl-cookie-jar" request-storage-directory)))
 
+(defvar request--curl-capabilities
+  (make-hash-table :test 'eq :weakness 'key)
+  "Cache table for capabilities plist of the curl at `request-curl'.
+:version     -- cURL's version string
+:compression -- non-nil if --compressed is supported")
+
+(defun request--curl-capabilities ()
+  "Return `request-curl' capabilites plist."
+  (let ((cache (gethash request-curl request--curl-capabilities)))
+    (if cache cache
+      (with-temp-buffer
+        (call-process "curl" nil t nil "--version")
+        (let ((version
+               (progn
+                 (goto-char (point-min))
+                 (when (re-search-forward "[.0-9]+" nil t)
+                   (match-string 0))))
+              (compression
+               (progn
+                 (goto-char (point-min))
+                 (not (null (re-search-forward "libz\\>" nil t))))))
+          (puthash request-curl `(:version ,version :compression ,compression)
+                   request--curl-capabilities))))))
+
 (defconst request--curl-write-out-template
   (if (eq system-type 'windows-nt)
       "\\n(:num-redirects %{num_redirects} :url-effective %{url_effective})"
@@ -905,12 +929,12 @@ Currently it is used only for testing.")
   (append
    (list request-curl "--silent" "--include"
          "--location"
-         ;; FIXME: test automatic decompression
-         "--compressed"
          ;; FIMXE: this way of using cookie might be problem when
          ;;        running multiple requests.
          "--cookie" cookie-jar "--cookie-jar" cookie-jar
          "--write-out" request--curl-write-out-template)
+   (when (plist-get (request--curl-capabilities) :compression)
+     '("--compressed"))
    request-curl-options
    (when unix-socket (list "--unix-socket" unix-socket))
    (cl-loop for (name filename path mime-type) in files*
