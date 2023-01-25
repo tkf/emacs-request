6bb8d2fa4904367dd4b87cce57d825587dea2c96
dickmao <none>
2023-01-25 18:40:53 -0500

    whitespace and comment

--- request.el
+++ request.el
@@ -913,7 +913,10 @@ BUG: Simultaneous requests are a known cause of cookie-jar corruption."
                                 (error (concat "request--curl-command-args: "
                                                "only one buffer or data entry permitted"))
                               (setq stdin-p t)))
-                          (list name (if (plist-get (cdr item) :use-contents) "<" "@") (or (plist-get (cdr item) :file) "-") (car item)
+                          (list name
+                                (if (plist-get (cdr item) :use-contents) "<" "@")
+                                (or (plist-get (cdr item) :file) "-")
+                                (car item)
                                 (if (plist-get (cdr item) :mime-type)
                                     (format ";type=%s" (plist-get (cdr item) :mime-type))
                                   "")))
@@ -947,9 +950,12 @@ posting fields, FILES containing one or more lists of the form
   (NAME . BUFFER)
   (NAME . (FILENAME :buffer BUFFER))
   (NAME . (FILENAME :data DATA))
+  (NAME . (FILENAME :data DATA :use-contents t))
 with NAME and FILENAME defined by curl(1)'s overwrought `--form` switch format,
 TIMEOUT in seconds, RESPONSE a mandatory struct, ENCODING, and SEMAPHORE,
-an internal semaphore.
+an internal semaphore.  Adding `:use-contents t` sends a text field
+with the file's contents as opposed to attaching a file as described
+in curl(1).
 
 Redirection handling strategy
 -----------------------------
